5d598f3ec8b262f7467028b331319d0e5db3c5e0
enriquefariasrdz <enriquefariasrdz@gmail.com>
2014-02-13 15:05:33 -0600

    Create env_1arg.sh

---
+++ env_1arg.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+env=$1
+
+
+
+for uuid in `cat modsource.txt`;
+do
+echo $uuid
+name=$(date +"%m_%d_%Y_%k%M")
+if [[ $1 == 'dev' || $1 == 'devhk' || $1 == 'devln' ||$1 == 'qa' || $1 == 'qaeds' || $1 == 'qahk' || $1 == 'qaln' || $1 == 'qatk' || $1 == 'bas' || $1 == 'bas2' || $1 == 'reg' || $1 == 'uat' || $1 == 'prod' || $1 == 'prodcredit' || $1 == 'prod2' || $1 == 'prod2genesis' || $1 == 'prod2hk' || $1 == 'prod2ln' || $1 == 'prod2tk' || $1 == 'prod2vi' ]] ; then
+
+
+/ms/dist/fiditoperations/PROJ/utils_optimus/prod/bin/log_finder_parallel.py --env $env  $uuid >>"$name-$$.txt"
+
+
+
+echo "File $name-$$.txt has been crated"
+else
+echo "****************************************Please check environment arguments******************************"
+
+fi
+
+done
