a743fe4f4a86d1a76e2e32bc94d8bce68a9d503b
enriquefariasrdz <enriquefariasrdz@gmail.com>
2014-02-13 20:38:44 -0600

    Create 1argument-v1.sh

---
+++ 1argument-v1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+env=$1
+
+
+
+for uuid in `cat modsource.txt`;
+do
+echo $uuid
+name=$(date +"%m_%d_%Y_%k%M")
+
+if [ $# == 1 ]; then
+
+
+/MSscriptparallel.py --env $env $uuid >>"$name-$$.txt"
+
+echo "File $name-$$.txt has been crated"
+else
+echo "****************************************Please check environment arguments******************************"
+
+fi
+
+done
